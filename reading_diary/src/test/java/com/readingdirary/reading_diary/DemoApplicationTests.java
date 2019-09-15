cf3709ff9dae70ae4567ad16040c300cc213641f
Zsolt M <hulfyback@gmail.com>
2019-09-15 12:02:23 +0200

    init (readingdiary): create project

---
+++ reading_diary/src/test/java/com/readingdirary/reading_diary/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.readingdirary.reading_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
